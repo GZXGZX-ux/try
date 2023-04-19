f0cdd7c91691efc20b7b0ae34de57f04b05c0cf5
GZXGZX-ux <2918750068@qq.com>
2023-04-19 21:39:34 +0800

    优化control函数中 的sql查询调用的方式，增加了一个各天报警案件趋势的路劲

--- apiController.js
+++ apiController.js
@@ -1,29 +1,30 @@
+const { promisify } = require('util');
 const pool = require('./db');
 
 exports.realtimeData = async (req, res, next) => {
   try {
-    pool.getConnection((err, connection) => {
-      if (err) throw err;
-      connection.query('SELECT * FROM ars', (error, results, fields) => {
-        connection.release(); // 释放连接，归还给连接池
-        if (error) throw error;
-        // eslint-disable-next-line no-use-before-define
-        processData(results);
-      });
+    const query = promisify(pool.query).bind(pool);
+    const result = await query('SELECT * FROM ars');
+    res.status(200).json({
+      status: 'success',
+      results: result.length,
+      data: { results: result },
     });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
+};
 
-    // eslint-disable-next-line no-inner-declarations
-    function processData(results) {
-      // 在这里处理 results
-      console.log(results);
-      res.status(200).json({
-        status: 'success',
-        results: results.length,
-        data: { results },
-      });
-    }
-
-    // res.json(result.recordset);
+exports.TrendOfTotalCasesPerDay = async (req, res, next) => {
+  try {
+    const query = promisify(pool.query).bind(pool);
+    const result = await query('SELECT *  FROM 8各天报警案件趋势');
+    res.status(200).json({
+      status: 'success',
+      results: result.length,
+      data: { results: result },
+    });
   } catch (error) {
     console.error(error);
     res.status(500).send('Server Error');
--- apiRouters.js
+++ apiRouters.js
@@ -3,4 +3,7 @@ const apiController = require('./apiController');
 
 const router = express.Router(); //make it be a middleware
 router.route('/realtimeData').get(apiController.realtimeData);
+router
+  .route('/TrendOfTotalCasesPerDay')
+  .get(apiController.TrendOfTotalCasesPerDay);
 module.exports = router;
