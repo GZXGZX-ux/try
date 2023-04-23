9b4b454b5bb1c81587aba5c0633155640589e9a1
GZXGZX-ux <2918750068@qq.com>
2023-04-23 12:55:02 +0800

    更改服务器端口号为配置文件端口号

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ dotenv.config({
   path: './config.env',
 });
 
-const port = 2000;
+const port = process.env.PORT;
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`App running on port ${port}...`);
