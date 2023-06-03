f4c83ce6c3828d78c30d9694222893df34868090
GZXGZX-ux <2918750068@qq.com>
2023-06-04 01:01:15 +0800

    修改前端代码删除精简

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ app.use(xss());
 //to prevent parameter pollution
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
-  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5583');
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5584');
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5585');
   res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5584');
   next();
 });
